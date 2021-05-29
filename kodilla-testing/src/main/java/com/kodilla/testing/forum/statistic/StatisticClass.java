3d4df2c81c4f0a63e17de0a64d48e3d22b6cbd9b
Tomek <tomek-dziewulski@wp.pl>
2021-05-29 09:48:07 +0200

    6.7 Zadanie Mockujemy forum dyskusyjne

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistic/StatisticClass.java
@@ -0,0 +1,65 @@
+package com.kodilla.testing.forum.statistic;
+
+import java.util.List;
+
+public class StatisticClass {
+
+    private int usersCount;
+    private int postsCount;
+    private int commentCount;
+    private int middlePostCount;
+    private int middleCommentCount;
+    private double middleCommentOfPostCount;
+    private double middlePostCountOfUsersCount;
+    private double middleCommentsOfUsersCount;
+    private List<String> userNames;
+
+
+    public void calculateAdvStatistics(Statistics statistics){
+        this.postsCount=statistics.postsCount();
+        this.commentCount = statistics.commentsCount();
+        if(statistics.usersNames()==null)
+            this.usersCount=0;
+        else
+        this.usersCount = statistics.usersNames().size();
+
+        this.middleCommentOfPostCount=statistics.commentsCount()*1.0/statistics.postsCount();
+
+       // this.middlePostCountOfUsersCount=statistics.postsCount()*1.0/statistics.usersNames().size();
+
+      //  this.middleCommentsOfUsersCount=statistics.commentsCount()*1.0/statistics.postsCount();
+
+
+    }
+    public void showStatistics(){
+
+    }
+
+    public int getUsersCount() {
+        return usersCount;
+    }
+
+    public int getPostsCount() {
+        return postsCount;
+    }
+
+    public int getCommentCount() {
+        return commentCount;
+    }
+
+    public int getMiddlePostCount() {
+        return middlePostCount;
+    }
+
+    public int getMiddleCommentCount() {
+        return middleCommentCount;
+    }
+
+    public double getMiddleCommentOfPostCount() {
+        return middleCommentOfPostCount;
+    }
+
+    public List<String> getUserNames() {
+        return userNames;
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/TestStatisticClass.java
@@ -0,0 +1,88 @@
+package com.kodilla.testing.forum.statistics;
+
+
+import com.kodilla.testing.forum.statistic.StatisticClass;
+import com.kodilla.testing.forum.statistic.Statistics;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TestStatisticClass {
+
+
+   @Mock
+   private static Statistics statisticMock=mock(Statistics.class);
+
+   @Test
+    void TestPostCount0(){
+       StatisticClass statisticClass = new StatisticClass();
+       when(statisticMock.postsCount()).thenReturn(0);
+       statisticClass.calculateAdvStatistics(statisticMock);
+       assertEquals(0,statisticClass.getPostsCount());
+
+   }
+
+   @Test
+   void TestPostCount1000(){
+      StatisticClass statisticClass = new StatisticClass();
+      when(statisticMock.postsCount()).thenReturn(1000);
+      statisticClass.calculateAdvStatistics(statisticMock);
+      assertEquals(1000,statisticClass.getPostsCount());
+   }
+   @Test
+   void TestCommentCount0(){
+      StatisticClass statisticClass = new StatisticClass();
+      when(statisticMock.commentsCount()).thenReturn(0);
+      statisticClass.calculateAdvStatistics(statisticMock);
+      assertEquals(0,statisticClass.getCommentCount());
+   }
+
+   @Test
+   void TestCommentCountLessThanPostCount(){
+    StatisticClass statisticClass = new StatisticClass();
+    when(statisticMock.commentsCount()).thenReturn(2);
+    when(statisticMock.postsCount()).thenReturn(8);
+    statisticClass.calculateAdvStatistics(statisticMock);
+    assertEquals(0.25,statisticClass.getMiddleCommentOfPostCount());
+   }
+   @Test
+   void TestCommentCountMoreThanPostCount(){
+      StatisticClass statisticClass= new StatisticClass();
+      when(statisticMock.commentsCount()).thenReturn(4);
+      when(statisticMock.postsCount()).thenReturn(2);
+      statisticClass.calculateAdvStatistics(statisticMock);
+      assertEquals(2,statisticClass.getMiddleCommentOfPostCount());
+   }
+   @Test
+   void uesrsCount0(){
+      StatisticClass statisticClass = new StatisticClass();
+      List<String> listUsers = null;
+
+      when(statisticMock.usersNames()).thenReturn(listUsers);
+      statisticClass.calculateAdvStatistics(statisticMock);
+      assertEquals(0,statisticClass.getUsersCount());
+   }
+
+   List<String>usersList(int i){
+      List<String>usersList=new ArrayList<>();
+      for(int a=0;a<i;a++){
+         usersList.add("Marcin"+a);
+      }
+      return usersList;
+   }
+   @Test
+   void usersCount100(){
+      StatisticClass statisticClass = new StatisticClass();
+      List<String>usersList;
+      usersList=usersList(100);
+      when(statisticMock.usersNames()).thenReturn(usersList);
+      statisticClass.calculateAdvStatistics(statisticMock);
+      assertEquals(100,statisticClass.getUsersCount());
+   }
+}
