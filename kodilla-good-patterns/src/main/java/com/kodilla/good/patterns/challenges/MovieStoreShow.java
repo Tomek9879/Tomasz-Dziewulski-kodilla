0b3b86a2197e6057c1838d274ea97ecab024eaa6
Tomek <tomek-dziewulski@wp.pl>
2021-08-10 22:18:36 +0200

    13 Funkcyjna iteracja po tytulach

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreShow.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreShow.java
@@ -2,14 +2,15 @@ package com.kodilla.good.patterns.challenges;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MovieStoreShow {
     public static void main(String [] args) {
         MovieStore movieStore = new MovieStore();
         Map<String, List<String>> mapaMovieStore = movieStore.getMovies();
 
-        mapaMovieStore.entrySet().stream().filter(e->e.getKey().equals("IM")|| e.getKey().equals("AV")).map(e->e.getValue()+"!").forEach(System.out::print);
-        mapaMovieStore.entrySet().stream().filter(e->e.getKey().equals("FL")).forEach(System.out::print)
+        String nazwa=  mapaMovieStore.entrySet().stream().map(e->e.getValue()).map(e->e.get(0)+"-"+e.get(1)).collect(Collectors.joining("!","",""));
+        System.out.println(nazwa);
     }
 }
 
